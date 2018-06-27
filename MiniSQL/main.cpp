bd4b5f60de6320b6fa4750fa9f36ca495848fadd
Vayne Tian <tzj912016486@vip.qq.com>
2018-06-27 09:09:28 +0800

    喵

--- MiniSQL/main.cpp
+++ MiniSQL/main.cpp
@@ -6,129 +6,54 @@ using namespace std;
 
 int main()
 {
-	//int mode;
-	//data_dictionary db;
-	//string input;
-	//cout << "*************************************" << endl;
-	//cout << "*    欢迎使用MiniSQL微型数据库      *" << endl;
-	//cout << "*        Date:2018-6-13             *" << endl;
-	//cout << "* 作者:田子珺，蒋文，林宇翔，沈子衿 *" << endl;
-	//cout << "*************************************" << endl;
-	//cout << "1. 语句执行模式" << endl;
-	//cout << "2. 脚本执行模式" << endl;
-	//cout << "3. 帮助" << endl;
-	//cout << "4. 退出本程序" << endl;
-	//while (true)
-	//{
-	//	cout << "请选择模式[1/2/3(帮助)/4]：";
-	//	cin >> mode;
-	//	getchar();
-	//	switch (mode)
-	//	{
-	//	case 1:
-	//	{
-	//		while (true)
-	//		{
-	//			input = Input();
-	//			string temp = input;
-	//			RemoveSpace(temp);
-	//			if (temp == "describe;")
-	//			{
-	//				db.listTable();
-	//			}
-	//			if (temp == "quit;")
-	//			{
-	//				break;
-	//			}
-	//			else
-	//			{
-	//				string result = Interpreter(input); //执行sql语句
-	//				if (result == "99")
-	//				{
-	//					cout << "语句执行错误！" << endl;
-	//				}
-	//				else if(result == "80")
-	//				{
-	//					cout << "语句执行正确" << endl;
-	//				}
-	//				else
-	//				{
-	//					cout << "语句执行错误！" << endl;
-	//				}
-	//			}
-	//		}
-	//		break;
-	//	}
-	//	case 2:
-	//	{
+	int md = 0;
+	if (md != 1) {
+		int mode;
+		data_dictionary db;
+		string input;
 
-	//	}
-	//	case 3:
-	//	{
-	//		cout << "*****操作手册****" << endl;
-	//		cout << "1. 语句执行模式" << endl;
-	//		cout << "2. 脚本执行模式" << endl;
-	//		cout << "3. 帮助" << endl;
-	//		cout << "4. 退出本程序" << endl;
-	//		cout << "*****************" << endl;
-	//		break;
-	//	}
-	//	case 4:
-	//	{
-	//		char iff;
-	//		cout << "你确定要退出吗? [Y/N]:";
-	//		cin >> iff;
-	//		if (iff == 'Y')
-	//		{
-	//			cout << "告辞! " << endl;
-	//			system("pause");
-	//			return 0;
-	//		}
-	//		else if (iff == 'N')
-	//		{}
-	//		break;
-	//	}
-	//	}
-	//}
-	//system("pause");
-	//return 0;
-	int mode;
-	data_dictionary db;
-	string input;
-	
-	while (true)
-	{
-		input = Input();
-		string temp = input;
-		RemoveSpace(temp);
-		if (temp == "describe;" || temp == "describe ;")
+		while (true)
 		{
-			db.db = new database();
-			db.listTable();
-		}
-		else if (temp == "quit;")
-		{
-			break;
-		}
-		else
-		{
-			string result = Interpreter(input); //执行sql语句
-			if (result == "99")
+			input = Input();
+			string temp = input;
+			RemoveSpace(temp);
+			if (temp == "describe;" || temp == "describe ;")
 			{
-				cout << "语句执行失败" << endl;
+				db.db = new database();
+				db.listTable();
 			}
-			else if (result == "80")
+			else if (temp == "quit;")
 			{
-				cout << "语句执行成功" << endl;
+				break;
 			}
 			else
 			{
-				cout << "语句执行失败" << endl;
+				string result = Interpreter(input); //执行sql语句
+				if (result == "99")
+				{
+					cout << "语句执行失败" << endl;
+				}
+				else if (result == "80")
+				{
+					cout << "语句执行成功" << endl;
+				}
+				else
+				{
+					cout << "语句执行失败" << endl;
+				}
 			}
 		}
+		system("pause");
+		return 0;
 	}
-	system("pause");
-	return 0;
+	else {
+		record_manager r;
+		for (int i = 0; i < 100; i++) {
+			r.select_2(m_string("t1"));
+			cout << i << endl;
+		}
+	
+	}
 }
 
 //int main() {
